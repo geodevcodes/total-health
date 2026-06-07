0eee55466fb10d69d4dfc289f6c2d45f23c86690
geodevcode <geodevinstitute@gmail.com>
2026-06-07 18:11:04 +0100

    packages updated

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -6,12 +6,6 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  constructor() {
-    super({
-      datasourceUrl: process.env.DATABASE_URL,
-    } as any);
-  }
-
   async onModuleInit() {
     await this.$connect();
   }
