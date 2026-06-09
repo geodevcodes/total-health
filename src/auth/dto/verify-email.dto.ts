974432295f6663769f884b50e3d0c15488e3c1dd
geodevcode <geodevinstitute@gmail.com>
2026-06-09 23:37:25 +0100

    Add ApiProperty decorator to VerifyEmailDto for improved Swagger documentation

--- src/auth/dto/verify-email.dto.ts
+++ src/auth/dto/verify-email.dto.ts
@@ -2,6 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class VerifyEmailDto {
+  @ApiProperty({ example: '447498' })
   @IsString()
   @IsNotEmpty()
   @Length(6, 6)
